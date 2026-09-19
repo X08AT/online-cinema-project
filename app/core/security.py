9ef4635c1bb5fb31f16407f0740adc6f0a782ad7
Artem Turchyn <Artem.at131@gmail.com>
2026-09-19 16:16:33 +0300

    Add create_access_token function

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,11 @@
+from datetime import datetime, timedelta, timezone
+
+from jose import jwt
 from pwdlib import PasswordHash
+
+from app.core.settings import settings
+
+ALGORITHM = "HS256"
 
 password_hash = PasswordHash.recommended()
 
@@ -8,3 +15,13 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed_password: str) -> bool:
     return password_hash.verify(password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
