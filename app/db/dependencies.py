85ea03ce18694f1362d1459a76b6ebd8c77c75cd
Artem Turchyn <Artem.at131@gmail.com>
2026-09-20 10:45:28 +0300

    Add get_current_user function

--- app/db/dependencies.py
+++ app/db/dependencies.py
@@ -1,10 +1,41 @@
 from typing import AsyncGenerator
 
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer
+from jose import jwt, JWTError
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.settings import settings
 from app.db.session import SessionLocal
+from app.models.user import User
+
+security = HTTPBearer()
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
         yield session
+
+
+async def get_current_user(credentials = Depends(security), db: AsyncSession = Depends(get_db)):
+    token = credentials.credentials
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid access token")
+
+    user_id = payload.get("sub")
+
+    if user_id is None:
+        raise HTTPException(status_code=401, detail="Invalid access token")
+
+    result = await db.execute(select(User).where(User.id == user_id))
+
+    user = result.scalar_one_or_none()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User does not exist")
+
+    return user
