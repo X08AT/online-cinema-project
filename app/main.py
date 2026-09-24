98ee1b36d594856b32a0ad4f6efe160e150c8ff5
Artem Turchyn <Artem.at131@gmail.com>
2026-09-24 11:16:35 +0300

    Connect admin router to app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from app.routers.auth import router as auth_router
+from app.routers.admin import router as admin_router
 
 app = FastAPI(title="Online Cinema", version="1.0")
 
 app.include_router(auth_router)
+app.include_router(admin_router)
