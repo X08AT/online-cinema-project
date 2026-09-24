a37d4a99fd276c010a9dfec650c1c80c21b6eedd
Artem Turchyn <Artem.at131@gmail.com>
2026-09-24 13:47:00 +0300

    Add profile update crud function

--- app/crud/profile.py
+++ app/crud/profile.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.user import UserProfile
-from app.schemas.profile import ProfileCreateModel
+from app.schemas.profile import ProfileCreateModel, ProfileUpdateModel
 
 
 async def get_profile_by_user_id(user_id: int, db: AsyncSession):
@@ -27,6 +27,28 @@ async def create_profile(
     )
 
     db.add(profile)
+
+    await db.commit()
+    await db.refresh(profile)
+
+    return profile
+
+
+async def update_profile(
+        user_id: int,
+        db: AsyncSession,
+        data: ProfileUpdateModel,
+        avatar: str | None = None,
+):
+    profile = await get_profile_by_user_id(user_id, db)
+
+    update_data = data.model_dump(exclude_unset=True)
+
+    for field, value in update_data.items():
+        setattr(profile, field, value)
+
+    if avatar is not None:
+        profile.avatar = avatar
 
     await db.commit()
     await db.refresh(profile)
